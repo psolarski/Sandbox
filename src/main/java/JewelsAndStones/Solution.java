a3425eb84451286bc767d37951f78e71e9c38a77
Piotr Solarski <piotrsolarski@outlook.com>
2018-06-03 22:48:36 +0200

    Addred solution for Jewels and Stones problem.

---
+++ src/main/java/JewelsAndStones/Solution.java
@@ -0,0 +1,23 @@
+package JewelsAndStones;
+
+public class Solution {
+
+    private static int numJewelsInStones(String J, String S) {
+        int c = 0;
+        for (int i=0; i<J.length(); i++) {
+            char cJ = J.charAt(i);
+            for (int j=0; j<S.length(); j++) {
+                if(cJ == S.charAt(j)) c++;
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        String J = "aA", S = "aAAbbbb";
+        System.out.println("Number of jewels: " + numJewelsInStones(J, S));
+
+        String Jewel = "z", Stones = "ZZ";
+        System.out.println("Number of jewels: " + numJewelsInStones(Jewel, Stones));
+    }
+}
