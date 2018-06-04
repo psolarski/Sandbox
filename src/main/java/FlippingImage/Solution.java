ef9084cbaca382c6018da97a0adeba0471deb5fd
Piotr Solarski <piotrsolarski@outlook.com>
2018-06-04 23:33:28 +0200

    Added solution for Flipping Image

---
+++ src/main/java/FlippingImage/Solution.java
@@ -0,0 +1,20 @@
+package FlippingImage;
+
+public class Solution {
+
+    private static int[][] flipAndInvertImage(int[][] A) {
+        int[][] newtable = new int[A.length][];
+        for (int i = 0; i < A.length; i++) {
+            newtable[i] = new int[A[i].length];
+            for (int j = 0; j < A[i].length; j++) {
+
+                if (A[i][j] == 1) A[i][j] = 0;
+                else A[i][j] = 1;
+            }
+            for (int j = 0; j < A[i].length; j++) {
+                newtable[i][j] = A[i][A[i].length - j - 1];
+            }
+        }
+        return newtable;
+    }
+}
