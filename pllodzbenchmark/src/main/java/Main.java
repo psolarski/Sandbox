7a04bd17bea359adbac9bc9e3057d10c97d1581a
Piotr Solarski <piotrsolarski@outlook.com>
2018-07-26 23:08:11 +0200

    Added additional benchmark parameters

--- pllodzbenchmark/src/main/java/Main.java
+++ pllodzbenchmark/src/main/java/Main.java
@@ -1,4 +1,7 @@
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
 
 public class Main {
 
@@ -11,6 +14,9 @@ public class Main {
     }
 
     @Benchmark
+    @Fork(value = 1) // test powtarzamy tylko dla jednego procesu
+    @Warmup(iterations = 1, time = 2) // jedna iteracja rozgrzewkowa trwająca 2 sekundy
+    @Measurement(iterations = 2, time = 2)
     public static int benchmark() throws InterruptedException {
         Thread.sleep(1000);
         System.out.print(1);
